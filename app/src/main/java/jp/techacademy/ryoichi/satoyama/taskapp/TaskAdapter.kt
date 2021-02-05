d8fa0ead87ebe52871c1803724265f5ae12d6404
ryoichi-satoyama <riyoichi_satoyama@mail.toyota.co.jp>
2021-02-05 17:18:41 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ryoichi/satoyama/taskapp/TaskAdapter.kt
@@ -0,0 +1,40 @@
+package jp.techacademy.ryoichi.satoyama.taskapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class TaskAdapter(context: Context) : BaseAdapter {
+    var mTaskList = mutableListOf<String>()
+    private val mLayoutInflater : LayoutInflater
+
+    init {
+        this.mLayoutInflater = LayoutInflater.from(context)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
+
+        val textView1 = view.findViewById<TextView>(android.R.id.text1)
+        val textView2 = view.findViewById<TextView>(android.R.id.text2)
+
+        textView1.text = mTaskList[position]
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return mTaskList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return mTaskList.size
+    }
+}
